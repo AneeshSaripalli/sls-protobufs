0ea72ce462687d03723c96821b6c19ce0cbe1b85
Aneesh Saripalli <aneeshsaripalli@gmail.com>
2021-01-30 13:21:01 -0600

    fix path resolution for book_pb

--- routes/handler.ts
+++ routes/handler.ts
@@ -1,15 +1,15 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import "source-map-support/register";
-import { Book } from "./protos/gen/book_pb.js";
+import { Book } from "./protos/gen/book_pb";
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
+export const hello: APIGatewayProxyHandler = async (_event, _context) => {
   const book: Book = new Book();
   book.setAuthor("Aneesh Saripalli");
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      response: "some body here",
+      response: book.toObject()
     }),
   };
 };
